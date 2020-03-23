5ea412453ec1af7ac035e6cc8229dd721259a20f
ghyoon93 <gh.yoon93@gmail.com>
2020-03-23 10:50:39 +0900

    imageboardDTO 추가

---
+++ src/main/java/imageboard/bean/ImageboardDTO.java
@@ -0,0 +1,19 @@
+package imageboard.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ImageboardDTO {
+    private int seq;
+    private String imageId;
+    private String imageName;
+    private int imagePrice;
+    private int imageQty;
+    private String imageContent;
+    private String image1;
+    private String logtime;
+}
